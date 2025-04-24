135bca94e90be71db064ddc42486933b4fdc2718
XWei1009 <Astronomy1009@gmail.com>
2025-04-24 10:08:26 +0800

    url.js

--- url.js
+++ url.js
@@ -1,4 +1,4 @@
 const urls = [
-  'https://forms.gle/RemhsRL32mXf9oed6',
-	'https://forms.gle/EuEYWaHC25VUM5VP8',
+  'https://www.surveycake.com/s/1MzZ7',
+	'https://www.surveycake.com/s/wwnqM',
 ];
